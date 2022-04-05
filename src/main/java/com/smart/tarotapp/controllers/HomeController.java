9e91290dc3a6eeb6810f937ecae00be2e52f5621
Nick Smart <nsmart147@gmail.com>
2022-04-05 01:26:38 -0400

    preparing for war packaging, setting up front/back end on same port

--- src/main/java/com/smart/tarotapp/controllers/HomeController.java
+++ src/main/java/com/smart/tarotapp/controllers/HomeController.java
@@ -1,26 +1,15 @@
 package com.smart.tarotapp.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
- 
-    @GetMapping("/")
-    public ModelAndView home() {
-        ModelAndView mav= new ModelAndView("index");
-        return mav;
+
+    @RequestMapping(value = {"/", "/cards", "/spreads"})
+    public String home() {
+        return "index.html";
     }
 
-    @RequestMapping(value = {"/cards"})
-    public String cards() {
-        return "index";
-    }
 
-    @RequestMapping(value = {"/spreads"})
-    public String spreads() {
-        return "index";
-    }
 }
